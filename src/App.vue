e04e6e4f3b8cc829b8322199b0df247c02784228
AhmedTx <63393122+AhmedTxl@users.noreply.github.com>
2023-10-02 09:45:37 +0300

    Revert "Initiate Repository"
    This reverts commit 68c8045ae7654c5ba7658cb39300d7c2720158b1.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
+
 <script>
-import Header from './components/Header.vue';
-import Main from './components/Main.vue';
+import Rating from './components/Rating.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,
-    Main,
+    Rating,
   }
 }
 </script>
 
 <template>
-  <Header />
-  <Main />
+  <Rating />
 </template>
 
 <style>
-@import "./main.css";
-
 #app {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
